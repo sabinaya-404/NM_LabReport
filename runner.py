508f710cb5d8a6773311db3f8b3b765d89c0a878
Sabinaya Khadka <sabinayakhadka3@gmail.com>
2026-09-15 15:30:27 +0545

    Add runner script

---
+++ runner.py
@@ -0,0 +1,11 @@
+import subprocess
+from pathlib import Path
+
+folder = Path(__file__).parent
+
+for file in sorted(folder.glob("*.py")):
+    if file.name == "runner.py":
+        continue
+
+    print(f"\n>>> python {file.name}")
+    subprocess.run(["python", str(file)])
